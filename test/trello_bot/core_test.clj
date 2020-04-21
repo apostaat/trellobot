4031af7882fdfda4d24a7ffaf0afba952cdcccef
apostaat <54319739+apostaat@users.noreply.github.com>
2020-04-21 23:00:19 +0300

    Update core_test.clj

--- test/trello_bot/core_test.clj
+++ test/trello_bot/core_test.clj
@@ -1,6 +1,7 @@
 (ns trello-bot.core-test
   (:require [clojure.test :refer :all]
             [trello-bot.core :refer :all]))
+; тестирование происходило в телеграме по большей части, посему тесты не написаны :( 
 
 (deftest a-test
   (testing "FIXME, I fail."
