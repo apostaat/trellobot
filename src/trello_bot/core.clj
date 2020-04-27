a8988aafe632eacd4481e153183c947e38892352
apostaat <54319739+apostaat@users.noreply.github.com>
2020-04-27 20:19:40 +0300

    Update core.clj

--- src/trello_bot/core.clj
+++ src/trello_bot/core.clj
@@ -1,4 +1,4 @@
-(ns trello-bot.core
+(ns trellotaskbot.core
   (:require [clojure.core.async :refer [<!!]]
             [clojure.string :as str]
             [environ.core :refer [env]]
@@ -7,15 +7,15 @@
             [morse.api :as t]
             [cheshire.core :refer :all :as json]
             [send-mail :as g ]
-            [clj-http.client :as http])
-            (:gen-class))
-  
+            [clj-http.client :as http]
+            [clojure.java.io :as io])
+  (:gen-class))
 
-(def token "secret ")
+(def token (env :telegram-token) )
 (def db (atom {}))
-(def db-file "pathtofile")
-(def e-mail "e-mail")
-(def secret "secret-password" )
+(def db-file (env :file-db))
+(def e-mail  (env :trello-mail))
+(def secret  (env :mail-password))
 (def base-url "https://api.telegram.org/bot")
 
 (when
@@ -25,7 +25,7 @@
 (defn e-mail? [text]
   (str/ends-with? text "@boards.trello.com" ))
 
-(defn old-user-logic [id new-or-old text]
+(defn get-old-user-task [id new-or-old text]
   (let [ split (str/split text #"\n")
         subject (first split)]
     (if (= 1 (count split))
@@ -33,9 +33,9 @@
                   Subject
                   Body of task")
       (do  (g/send-mail e-mail new-or-old subject (apply str (rest split))  secret )
-           (t/send-text token id (str "E-mail was sent to " new-or-old "Subject: " subject "Body: " (rest split) "."))))))
+           (t/send-text token id (str "E-mail was sent to " new-or-old "Subject: " subject " Body: " (rest split) "."))))))
 
-(defn new-user-logic [text id]
+(defn get-new-user-mail [text id]
   (if (e-mail? text)
     (do (swap! db assoc (str id) text )
         (t/send-text token id (str "This e-mail: " text " will be used as for sending tasks to Trello."))
@@ -68,24 +68,30 @@
   (h/command-fn "help"
     (fn [{{id :id :as chat} :chat}]
       (println "Help was requested in " chat)
-      (send-url-button token id)))
+      (t/send-text token id
+                  {:reply_markup {:inline_keyboard
+                                  [[{:text "How to create Trello board e-mail (official docs)."
+                                     :url "https://help.trello.com/article/809-creating-cards-by-email"
+                                     }]]}}
+                   "To create Trello e-mail use the link below. \n To create task in TrelloBot:\n Taskname \n Then enter task body using newline."
+                   )))
 
   (h/command-fn "forgetme"
       (fn [{{id :id}  :chat :as message}]
-      (println "This user has been forgotten" (:id (:from message)))
       (do (t/send-photo token id (io/file (io/resource "a.png")))
           (swap! db dissoc (str id))
-          (spit db-file @db)
-          (http/post (str base-url token "/getUpdates?offset=" (apply :offset (:entities message)))))))
+          (spit db-file @db))))
 
   (h/message-fn
     (fn [{{id :id}  :chat :as message}]
       (let [text (:text message)
             new-or-old  (get @db (str id))]
-        (println "Intercepted message: " message)
-        (if new-or-old
-          (old-user-logic id new-or-old text)
-          (new-user-logic text id))))))
+        (if (= text "/forgetme")
+          (println "This user has been forgotten" (:username (:from message)))
+          (do (println "Intercepted message: " message)
+              (if new-or-old
+                (get-old-user-task id new-or-old text)
+                (get-new-user-mail text id))) )))))
 
 (defn -main
   [& args]
@@ -98,3 +104,4 @@
 
 (def wow (p/start token handler))
 (clojure.core.async/close! wow)
+
